b139230db2e8158564f41d10f64b3bafd7773794
jmlc643 <josemarialuyocampos@gmail.com>
2026-07-26 21:06:15 -0500

    feat: Primera version del login y perfil del frontend

---
+++ src/store/useAuthStore.ts
@@ -0,0 +1,37 @@
+import { create } from 'zustand';
+import Cookies from 'js-cookie';
+import type { User } from '@/features/auth/types/auth.types';
+
+const TOKEN_KEY = 'twitbet_token';
+
+interface AuthState {
+  user: User | null;
+  token: string | null;
+  isAuthenticated: boolean;
+  setAuth: (user: User, token: string) => void;
+  logout: () => void;
+  initAuth: () => void;
+}
+
+export const useAuthStore = create<AuthState>((set) => ({
+  user: null,
+  token: Cookies.get(TOKEN_KEY) || null,
+  isAuthenticated: !!Cookies.get(TOKEN_KEY),
+
+  setAuth: (user, token) => {
+    Cookies.set(TOKEN_KEY, token, { expires: 3, secure: true, sameSite: 'lax' });
+    set({ user, token, isAuthenticated: true });
+  },
+
+  logout: () => {
+    Cookies.remove(TOKEN_KEY);
+    set({ user: null, token: null, isAuthenticated: false });
+  },
+
+  initAuth: () => {
+    const token = Cookies.get(TOKEN_KEY);
+    if (!token) {
+      set({ user: null, token: null, isAuthenticated: false });
+    }
+  },
+}));
